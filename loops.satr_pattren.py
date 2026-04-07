f0950760f4e5fc87d4994a6e93d84851f450b119
gr5175777-del <gr5175777@gmail.com>
2026-04-07 11:46:59 +0530

    Add files via upload

---
+++ loops.satr_pattren.py
@@ -0,0 +1,15 @@
+'''def star_pattren(n):
+    for i in range(1,n+1):
+        for j in range(i):
+            print("*",end="")
+        print()
+star_pattren(5)'''
+
+
+#space bettwen the stars
+def star_pattren(n):
+    for i in range(1,n+1):
+        for j in range(i):
+            print("*",end=" ")
+        print()
+star_pattren(5)
